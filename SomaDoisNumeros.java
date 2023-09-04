73e8eca3b2f409a799c1ca919daa8b18fe7fe3eb
Kevin Rodrigues Garcia - 823155644 <kevigarcia5644@aluno.saojudas.br>
2023-09-04 20:41:55 -0300

    ap02 (input-output) : resultado ok

--- SomaDoisNumeros.java
+++ SomaDoisNumeros.java
@@ -1,16 +1,22 @@
 import java.util.Scanner;
 //pacote padrão: java.lang
 public class SomaDoisNumeros {
-public static void main (String {} args) {
-//declaração de variável
-Scanner leitor = new Scanner (System.in); 
-double primeiroOperando;
-double segundoOperando;
-double resultado;
+    public static void main (String [] args) {
+        //declaração de variável
+        Scanner leitor = new Scanner (System.in); 
+        double primeiroOperando;
+        double segundoOperando;
+        double resultado;
 
-//entrada
+        //entrada
+        System.out.println("Digite o primeiro operando");
+        primeiroOperando = leitor.nextDouble();
+        System.out.println("Digite o segundo operando");
+        segundoOperando = leitor.nextDouble();
+        //processamento
+        resultado = primeiroOperando + segundoOperando;
 
-//processamento
-
-//saída
+        //saída
+        System.out.print(resultado);
+    }
 }
